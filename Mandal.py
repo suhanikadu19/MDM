f830aad90e397f86fb81ec4b9d63215281eecd27
suhanikadu19 <suhanikadu07@gmail.com>
2026-07-26 19:56:05 +0530

    Add files via upload

---
+++ Mandal.py
@@ -0,0 +1,15 @@
+from turtle import*
+speed(0)
+bgcolor("black")
+setposition(-4,-2)
+color("red")
+tracer(3,0)
+hideturtle()
+for i in range(500):
+  rt(i)
+  circle(150,i)
+  fd(70)
+  right(210)
+  fd(i)
+  lt(1)
+done()
